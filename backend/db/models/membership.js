2a9572068f7df520df934133a3cae7e922e043d8
owencshoop <owencshoop@gmail.com>
2022-11-23 12:52:05 -0800

    refactor api response for add eventimage

--- backend/db/models/membership.js
+++ backend/db/models/membership.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Membership.belongsTo(models.Group, {foreignKey: 'groupId'})
-      Membership.belongsTo(models.User, {foreignKey: 'userId', as: 'memberId'})
+      Membership.belongsTo(models.User, {foreignKey: 'userId'})
     }
   }
   Membership.init({
