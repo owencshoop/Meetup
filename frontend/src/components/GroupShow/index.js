340535a99805ff562a8fcebe77fbd901fc3df5b3
owencshoop <owencshoop@gmail.com>
2022-12-14 11:07:03 -0800

    add basic styling and positioning for group show page

--- frontend/src/components/GroupShow/index.js
+++ frontend/src/components/GroupShow/index.js
@@ -29,62 +29,83 @@ const GroupShow = () => {
   return (
     <div className="group-show-container">
       {isLoaded && (
-          <>
-            <div className="group-show-image-name-container">
-              <div className="group-show-image-container">
-                <img
-                  className="group-show-image"
-                  src={
-                    group.GroupImages.length > 0
-                      ? `${group.GroupImages[0].url}`
-                      : ""
-                  }
-                  alt={`${group.name}'s Preview'`}
-                ></img>
-              </div>
-              <div className="group-show-name-container">
-                <div>Name: {group.name}</div>
-                <div>
-                  Organizer: {group.Organizer.firstName}{" "}
-                  {group.Organizer.lastName}
+        <>
+          <div className="group-show-image-name-container">
+            <div className="group-show-image-container">
+              <img
+                className="group-show-image"
+                src={
+                  group.GroupImages.length > 0
+                    ? `${group.GroupImages[0].url}`
+                    : ""
+                }
+                alt={`${group.name}'s Preview'`}
+              ></img>
+            </div>
+            <div className="group-show-name-container">
+              <h1 className="group-show-name">{group.name}</h1>
+              <div className="group-show-location-members-organizer">
+                <div className="group-show-name-content">
+                  <div className="group-show-name-icons">
+                    <i class="fa-solid fa-location-dot"></i>
+                  </div>
+                  <div>
+                    {group.city}, {group.state}
+                  </div>
                 </div>
-                <div>
-                  Location: {group.city}, {group.state}
+                <div className="group-show-name-content">
+                  <div className="group-show-name-icons">
+                    <i class="fa-solid fa-user-group"></i>
+                  </div>
+                  <div>
+                    {group.numMembers}{" "}
+                    {group.numMembers > 1 ? "members" : "member"} •{" "}
+                    {group.private ? "Private" : "Public"} group{" "}
+                  </div>
                 </div>
-                <div>{group.type}</div>
-                <div>Members: {group.numMembers}</div>
-                <div>{group.private ? "Private" : "Public"}</div>
+                <div className="group-show-name-content">
+                  <div className="group-show-name-icons">
+                    <i class="fa-solid fa-user"></i>
+                  </div>
+                  <div>
+                    Organized by{" "}
+                    <span className="group-show-name-organizer">
+                      {group.Organizer.firstName} {group.Organizer.lastName}
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="group-show-body-container">
-              <div className="group-show-about-container">
-                <div>Description: {group.about}</div>
+          </div>
+          <div className="group-show-body-container">
+            <div className="group-show-about-container">
+              <div>Description: {group.about}</div>
+            </div>
+            <div className="group-show-organizer-member-container">
+              <div className="group-show-organizer-container">
+                Organizer: {group.Organizer.firstName}{" "}
+                {group.Organizer.lastName}
               </div>
-              <div className="group-show-organizer-member-container">
-                <div className="group-show-organizer-container">
-                  Organizer: {group.Organizer.firstName}{" "}
-                  {group.Organizer.lastName}
-                </div>
-              </div>
+            </div>
+          </div>
+          <div className="group-show-button-container">
+            <button onClick={(e) => handleGroupDelete(e, group.id)}>
+              Delete Group
+            </button>
+            <div>
+              <OpenEditGroupFormModal
+                itemText="Edit Group"
+                modalComponent={<EditGroupFormModal group={group} />}
+              />
             </div>
             <div>
-              <button onClick={(e) => handleGroupDelete(e, group.id)}>
-                Delete Group
-              </button>
-              <div>
-                <OpenEditGroupFormModal
-                  itemText="Edit Group"
-                  modalComponent={<EditGroupFormModal group={group} />}
-                />
-              </div>
-              <div>
-                <OpenCreateEventModal
-                  itemText="Create an Event"
-                  modalComponent={<CreateEventFormModal group={group} />}
-                />
-              </div>
+              <OpenCreateEventModal
+                itemText="Create an Event"
+                modalComponent={<CreateEventFormModal group={group} />}
+              />
             </div>
-          </>
+          </div>
+        </>
       )}
     </div>
   );
