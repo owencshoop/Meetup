354a963bb41e262eef329f073d66b69d44f1da51
owencshoop <owencshoop@gmail.com>
2022-12-16 10:50:02 -0800

    style create group footer button

--- frontend/src/components/FooterContainer/OpenCreateGroupModal.js
+++ frontend/src/components/FooterContainer/OpenCreateGroupModal.js
@@ -16,7 +16,7 @@ function OpenCreateGroupModal({
   };
 
   return (
-    <div className='footer-modal-button' onClick={onClick}>{itemText}</div>
+    <div className='footer-modal-button-create-group' onClick={onClick}>{itemText}</div>
   );
 }
 
